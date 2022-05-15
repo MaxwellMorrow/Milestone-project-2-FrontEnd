00b2d390eec5da165f34cdc9b6d237b6c603c980
Clayton <claytongorham@gmail.com>
2022-05-15 11:07:20 -0700

    add MVC files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+// import Dashboard from "./layouts/Dashboard";
+// import Teams from "./layouts/Teams";
+// import Employees from "./layouts/Employees";
+// import NewEmployee from "./layouts/NewEmployee";
+// import EditEmployee from "./layouts/EditEmployee";
+// import NotFound from "./layouts/NotFound";
+import { Route, Switch } from "react-router-dom";
 
-function App() {
+export default function App() {
+  const [employees, setEmployees] = useState([]);
+
+  // const [teams, setTeams] = useState([
+  //   { TeamName: "Executive", Employees: 5, Budget: 50000, Actual: 40000 },
+  //   { TeamName: "Marketing", Employees: 5, Budget: 50000, Actual: 40000 },
+  //   { TeamName: "Technology", Employees: 5, Budget: 100000, Actual: 40000 },
+  //   { TeamName: "Accounting", Employees: 5, Budget: 50000, Actual: 40000 },
+  // ]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/")
+      .then((response) => response.json())
+      .then((data) => {
+        setEmployees(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path='/' render={() => <Dashboard teams={teams} employees={employees} />} />
+      <Route exact path='/Teams' render={() => <Teams employees={employees} teams={teams} />} />
+      <Route exact path='/Employees' render={() => <Employees employees={employees} />} />
+      <Route exact path='/NewEmployee' render={() => <NewEmployee />} />
+      <Route exact path='/EditEmployee' render={() => <EditEmployee />} />
+      <Route render={() => <NotFound />} />
+    </Switch>
   );
 }
-
-export default App;
